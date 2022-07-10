9f62e00866dc27fd4303549dc4122497c288ea38
Peng Lee <lishuaipeng651@gmail.com>
2022-07-10 18:14:46 +0800

    change build orders

--- build-docker.sh
+++ build-docker.sh
@@ -11,10 +11,6 @@ docker exec master bash -c "echo '$SERVER_PUB_KEY' > /root/.ssh/authorized_keys"
 docker exec master /bin/systemctl enable sshd
 docker exec master /bin/systemctl start sshd
 
-echo -e "\033[32mStarting deploy bigtop cluster master\033[0m"
-docker exec master bash -c "puppet apply --detailed-exitcodes --parser future --hiera_config=/etc/puppet/hiera.yaml --modulepath=/bigtop_home/bigtop-deploy/puppet/modules:/etc/puppet/modules:/usr/share/puppet/modules:/etc/puppetlabs/code/modules:/etc/puppet/code/modules /bigtop_home/bigtop-deploy/puppet/manifests"
-
-
 echo -e "\033[32mCreating docker cluster worker01\033[0m"
 docker run -d --name worker01 --hostname worker01 --network bigtop --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro bigtop:3.1.0 /usr/sbin/init
 
@@ -31,6 +27,9 @@ docker exec worker01 bash -c "echo '$MASTER_IP      ambari-server' >> /etc/hosts
 docker exec worker01 bash -c "echo '$WORKER02_IP      worker02' >> /etc/hosts"
 docker exec worker02 bash -c "echo '$MASTER_IP      ambari-server' >> /etc/hosts"
 docker exec worker02 bash -c "echo '$WORKER01_IP      worker01' >> /etc/hosts"
+
+echo -e "\033[32mStarting deploy bigtop cluster master\033[0m"
+docker exec master bash -c "puppet apply --detailed-exitcodes --parser future --hiera_config=/etc/puppet/hiera.yaml --modulepath=/bigtop_home/bigtop-deploy/puppet/modules:/etc/puppet/modules:/usr/share/puppet/modules:/etc/puppetlabs/code/modules:/etc/puppet/code/modules /bigtop_home/bigtop-deploy/puppet/manifests"
 
 echo -e "\033[32mStarting deploy bigtop cluster worker01\033[0m"
 docker exec worker01 bash -c "puppet apply --detailed-exitcodes --parser future --hiera_config=/etc/puppet/hiera.yaml --modulepath=/bigtop_home/bigtop-deploy/puppet/modules:/etc/puppet/modules:/usr/share/puppet/modules:/etc/puppetlabs/code/modules:/etc/puppet/code/modules /bigtop_home/bigtop-deploy/puppet/manifests"
