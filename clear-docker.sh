f1421bbe2f3dc4b8a7762333de5b497c0472f157
Peng Lee <lishuaipeng651@gmail.com>
2022-07-11 19:00:40 +0800

    short the command

--- clear-docker.sh
+++ clear-docker.sh
@@ -10,8 +10,8 @@ docker rm -f worker02
 echo -e "\033[32mRemoving network bigtop\033[0m"
 docker network rm bigtop
 
-# echo -e "\033[32mRemoving image ambari-server:2.7.5\033[0m"
-# docker rmi ambari-server:2.7.5
+echo -e "\033[32mRemoving image bigtop:3.1.0\033[0m"
+docker rmi bigtop:3.1.0
 
 # echo -e "\033[32mRemoving image ambari-agent:2.7.5\033[0m"
 # docker rmi ambari-agent:2.7.5
---
+++ start-build.sh
@@ -0,0 +1,2 @@
+echo -e "\033[32mBuild bigtop image\033[0m"
+docker build -t bigtop:3.1.0 .
