59824234c9866cff7d9cb435482f06ba96a96bd8
Peng Lee <lishuaipeng651@gmail.com>
2022-07-12 21:04:16 +0800

    fix serious error and add hue

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
-# echo -e "\033[32mBuilding image ambari:2.7.5\033[0m"
-# docker build -t ambari:2.7.5 .
+# echo -e "\033[32mBuilding image bigtop:3.1.0\033[0m"
+# docker build -t bigtop:3.1.0.
 
 echo -e "\033[32mCreating network bigtop\033[0m"
 docker network create --driver bridge bigtop
@@ -17,16 +17,24 @@ docker run -d --name worker01 --hostname worker01 --network bigtop --privileged 
 echo -e "\033[32mCreating docker cluster worker02\033[0m"
 docker run -d --name worker02 --hostname worker02 --network bigtop --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro bigtop:3.1.0 /usr/sbin/init
 
+echo -e "\033[32mCreating docker cluster hue\033[0m"
+docker run -d --name hue --hostname hue --network bigtop --privileged -e "container=docker" hue:4.10.1
+
 echo -e "\033[32mConfiguring hosts file\033[0m"
 MASTER_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' master`
 WORKER01_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' worker01`
 WORKER02_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' worker02`
 docker exec master bash -c "echo '$WORKER01_IP      worker01' >> /etc/hosts"
 docker exec master bash -c "echo '$WORKER02_IP      worker02' >> /etc/hosts"
-docker exec worker01 bash -c "echo '$MASTER_IP      ambari-server' >> /etc/hosts"
+docker exec worker01 bash -c "echo '$MASTER_IP      master' >> /etc/hosts"
 docker exec worker01 bash -c "echo '$WORKER02_IP      worker02' >> /etc/hosts"
-docker exec worker02 bash -c "echo '$MASTER_IP      ambari-server' >> /etc/hosts"
+docker exec worker02 bash -c "echo '$MASTER_IP      master' >> /etc/hosts"
 docker exec worker02 bash -c "echo '$WORKER01_IP      worker01' >> /etc/hosts"
+
+echo -e "\033[32mConfiguring hue hosts file\033[0m"
+docker exec hue bash -c "echo '$MASTER_IP      master' >> /etc/hosts"
+docker exec hue bash -c "echo '$WORKER01_IP      worker01' >> /etc/hosts"
+docker exec hue bash -c "echo '$WORKER02_IP      worker02' >> /etc/hosts"
 
 echo -e "\033[32mStarting deploy bigtop cluster worker01\033[0m"
 docker exec worker01 bash -c "puppet apply --detailed-exitcodes --parser future --hiera_config=/etc/puppet/hiera.yaml --modulepath=/bigtop_home/bigtop-deploy/puppet/modules:/etc/puppet/modules:/usr/share/puppet/modules:/etc/puppetlabs/code/modules:/etc/puppet/code/modules /bigtop_home/bigtop-deploy/puppet/manifests"
