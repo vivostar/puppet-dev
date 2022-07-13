00c3ec1a94a430f52adc6d62122121d9569cfbba
Peng Lee <lishuaipeng651@gmail.com>
2022-07-13 19:41:13 +0800

    change the order of checkouk

--- hue-build-docker.sh
+++ hue-build-docker.sh
@@ -1,3 +1,4 @@
 echo -e "\033[32mBuild hue docker\033[0m"
+(cd ~/hue; git checkout release-4.10.0)
 (cd ~; echo y | cp -f hue-config/hue.ini hue/desktop/conf.dist)
-(cd ~/hue; git checkout release-4.10.0; docker build -t hue:4.10.1 -f tools/docker/hue/Dockerfile .)
+(cd ~/hue; docker build -t hue:4.10.1 -f tools/docker/hue/Dockerfile .)
