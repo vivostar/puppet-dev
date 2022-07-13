5c9ea2bb021f54c1e2b1f31c8d8aba5e232c3f41
Peng Lee <lishuaipeng651@gmail.com>
2022-07-13 19:11:07 +0800

    typos

--- update-node.sh
+++ update-node.sh
@@ -3,5 +3,5 @@ echo -e "\033[32mUpdate hieradata\033[0m"
 docker exec $1 bash -c "(cd /bigtop_home; git pull origin master)"
 echo -e "\033[32mUpdate hieradata\033[0m"
 docker exec $1 rsync -a --delete /bigtop_home/bigtop-deploy/puppet/hieradata /etc/puppet
-echo -e "\033[32mStarting deploy bigtop cluster master\033[0m"
+echo -e "\033[32mStarting deploy bigtop cluster $1\033[0m"
 docker exec $1 bash -c "puppet apply --detailed-exitcodes --parser future --hiera_config=/etc/puppet/hiera.yaml --modulepath=/bigtop_home/bigtop-deploy/puppet/modules:/etc/puppet/modules:/usr/share/puppet/modules:/etc/puppetlabs/code/modules:/etc/puppet/code/modules /bigtop_home/bigtop-deploy/puppet/manifests"
